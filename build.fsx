de47f1362b3df3df266692370899f8674bb03b93
Jared Hester <jared.hester@gmail.com>
2016-08-20 04:18:09 -0400

    adjust build strategy

--- build.fsx
+++ build.fsx
@@ -1,6 +1,6 @@
-//--------------------------------------------------------//
-// Ionide-FsGrammar - FSharp.SyntaxTest FAKE Build Script //
-//--------------------------------------------------------//
+//---------------------------------------------------------//
+// Ionide-FsGrammar - FSharp.SyntaxTools FAKE Build Script //
+//---------------------------------------------------------//
 
 // This build script is run inside VSCode when `F5` is used to Launch the Extension
 
@@ -18,6 +18,27 @@ let syntaxDir           = "grammar"
 let extensionDir        = "fsharp.syntaxtest"
 /// FSharp-SyntaxTest will load the Syntax Definition files in this dir    
 let extensionSyntaxDir  = extensionDir</>"syntaxes"
+/// this files is created as a flag upon successful npm installation,
+/// to prevent it from re-running every build
+let setupcomplete = "packages"</>"built.md" 
+
+
+let platformTool tool path =
+    isUnix |> function | true -> tool | _ -> path
+
+/// Node Package Manager
+let npmTool =    
+    platformTool "npm" ("packages"</>"Npm.js"</>"tools"</>"npm.cmd"|>FullName)
+
+let run cmd args workingDir =
+    match  execProcess (fun info ->
+        if not (String.IsNullOrWhiteSpace workingDir) then 
+            info.WorkingDirectory <- workingDir
+        info.FileName <- cmd    
+        info.Arguments <- args
+        ) System.TimeSpan.MaxValue with
+    | false -> traceError <| sprintf "Error while running '%s' with args: %s" cmd args; false
+    | true -> true
 
 
 let CopyGrammar & cg = "CopyGrammar" 
@@ -26,57 +47,42 @@ Target cg  (fun _ ->
     ensureDirectory extensionSyntaxDir
     CleanDir extensionSyntaxDir
     CopyFiles extensionSyntaxDir [
-        syntaxDir </> "fsharp.fsi.json"
-        syntaxDir </> "fsharp.fsl.json"
-        syntaxDir </> "fsharp.fsx.json"
-        syntaxDir </> "fsharp.json"
+        syntaxDir</>"fsharp.fsi.json"
+        syntaxDir</>"fsharp.fsl.json"
+        syntaxDir</>"fsharp.fsx.json"
+        syntaxDir</>"fsharp.json"
     ]
 )
 
-let platformTool tool path =
-    isUnix |> function | true -> tool | _ -> path
 
-/// Node Package Manager
-let npmTool =    
-    platformTool "npm" ("packages"</>"Npm.js"</>"tools"</>"npm.cmd" |> FullName)
-
-
-let run cmd args workingDir =
-    if  execProcess (fun info ->
-        if not (String.IsNullOrWhiteSpace workingDir) then 
-            info.WorkingDirectory <- workingDir
-        info.FileName <- cmd    
-        info.Arguments <- args
-        ) System.TimeSpan.MaxValue = false then
-        traceError <| sprintf "Error while running '%s' with args: %s" cmd args
-
-
-let BuildExtension & bext = "BuildExtension" 
-Target bext  (fun () ->
-    trace "Building VSCode Extension - FSharp-SyntaxTest"
+let InstallDependencies & dep = "InstallDependencies" 
+Target dep  (fun () ->
+    trace "Installing FSharp.SyntaxTools npm packages"
     // install any necessary dependencies for the FSharp-SyntaxTest Extension
-    run npmTool "install --verbose" extensionDir   
+    match run npmTool "install --verbose" extensionDir with
+    | false -> DeleteFile setupcomplete
+    | true  -> CreateFile setupcomplete
 )
 
-
-let Notify & nt = "Notify" 
-Target nt  (fun () ->
-    traceLine ()
-    traceError <|
-        sprintf"\n\
-        This Build Script is not intended to be run from the commandline\n\n\
-        Launch VSCode with \n\
-        -   code %s\n\n\
-        and start the extension host with `F5` to run FSharp.SyntaxTest\n\n" __SOURCE_DIRECTORY__
-    traceLine ()
+let FableCompile & fc = "FableCompile" 
+Target fc  (fun () ->
+    trace "Transpile FSharp.SyntaxTools to Javascript"
+    run npmTool "run-script build" extensionDir |> ignore 
 )
+
 
 
 /// Visual Studio Code
 let codeTool =
     platformTool "code" (ProgramFilesX86</>"Microsoft VS Code"</>"bin/code.cmd")
 
+let needsInstall = not <| fileExists setupcomplete
 
-CopyGrammar ==> BuildExtension
+CopyGrammar 
+ ==> InstallDependencies
+ =?> (FableCompile, needsInstall) 
 
-RunTargetOrDefault BuildExtension 
+CopyGrammar 
+ ==> FableCompile
+
+RunTargetOrDefault FableCompile 
--- src/syntaxtool.fs
+++ src/syntaxtool.fs
@@ -1,4 +1,14 @@
-namespace SyntaxTools
+namespace Ionide.SyntaxTools
+
+open FSharp
+open Fable.Core
+open Fable.Import.JS
+open Fable.Import.Node
+open Fable.Import.vscode
+open Fable.Import.textmate
+open Ionide.VSCode
+open Ionide.VSCode.Helpers
+
 
 module Core =
     
